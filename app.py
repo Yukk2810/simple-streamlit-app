a121a66a97db381d67600afa7642e585cd1bb6c3
Yukk2810 <52900330+Yukk2810@users.noreply.github.com>
2023-09-30 10:09:39 +0000

    プロンプトやタイトルやインプットを変更

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ openai.api_base = "https://oai.langcore.org/v1"
 
 def main():
     st.title("ボトルネック見つけるくん")
-    user_input = st.text_input("キャッチコピーを作成するためのキーワードを入力してください: ")
-    if st.button("キャッチコピーを生成"):
+    user_input = st.text_input("課題や悩んでいる内容を入力してください: ")
+    if st.button("ボトルネックを調べる"):
         catchphrase = call_chatgpt_api(user_input)
-        st.write("生成されたキャッチコピー: ", catchphrase)
+        st.write("考えられるボトルネック: ", catchphrase)
 
 
 def call_chatgpt_api(input_text):
     try:
-        prompt = f"""次のお題からキャッチコピーを作成してください。
-お題: {input_text}
+        prompt = f"""あなたは経営戦略に秀でた製造エンジニアで、データドリブンの思考を持っています。次の内容からボトルネックを考えてください。
+内容: {input_text}
 """
         response = openai.Completion.create(
             model="gpt-3.5-turbo-instruct",
@@ -25,7 +25,7 @@ def call_chatgpt_api(input_text):
         )
         return response.choices[0].text.strip()
     except Exception as e:
-        return f"エラー: キャッチコピーを生成できませんでした。{str(e)}"
+        return f"エラー: ボトルネックは分かりませんでした。{str(e)}"
 
 
 if __name__ == "__main__":
